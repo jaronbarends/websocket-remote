f01b8732b761e8e8a9d71fe9647faea8cb3606c4
jaronbarends <jaron@jaron.nl>
2015-08-28 09:45:40 +0200

    structured server code into functions

--- public/js/socket.js
+++ public/js/socket.js
@@ -2,22 +2,29 @@
 
 	'use strict';
 
+	// define semi-global variables (vars that are "global" in this file's scope) and prefix them
+	// with sg so we can easily distinguish them from "normal" vars
+	var sgSocket;
+
+
+	/**
+	* handle server's connectionready event
+	* @returns {undefined}
+	*/
+	var connectionreadyHandler = function() {
+		console.log('socket.js - connection ready');
+		$(document).trigger('connectionready.socket', sgSocket);
+	};
+	
+
 	/**
 	* initialize the socket, and send event containing it to the page
 	* @param {string} varname Description
 	* @returns {undefined}
 	*/
 	var initSocket = function() {
-		var socket = io(),
-			data = {
-				socket: socket
-			};
-
-
-		socket.on('connectionready', function() {
-			console.log('socket.js - connection ready');
-			$(document).trigger('connectionready.socket', socket);
-		});
+		sgSocket = io();
+		sgSocket.on('connectionready', connectionreadyHandler);
 	};
 	
 
--- socket-server.js
+++ socket-server.js
@@ -1,63 +1,106 @@
 // This is the server-side file of our mobile remote controller app.
 // It initializes socket.io and a new express instance.
-// Start it by running 'node app.js' from your terminal.
+// Start it by running 'node socket-server' from your terminal.
 
 
-// Creating an express server
+// Define global vars
+var express,
+	app,
+	port,
+	io,
+	room;
 
-var express = require('express'),
+
+/**
+* initialize basic requirements for the server
+* @returns {undefined}
+*/
+var initBasicRequirements = function() {
+
+	//create express server
+	express = require('express');
 	app = express();
 
-// This is needed if the app is run on heroku and other cloud providers:
+	//set port that we'll use
+	port = process.env.PORT || 8000;// This is needed if the app is run on heroku and other cloud providers:
 
-var port = process.env.PORT || 8000;
+	// Initialize a new socket.io object. It is bound to 
+	// the express app, which allows them to coexist.
+	io = require('socket.io').listen(app.listen(port));
 
-// Initialize a new socket.io object. It is bound to 
-// the express app, which allows them to coexist.
-
-var io = require('socket.io').listen(app.listen(port));
-
-
-// App Configuration
-
-// Make the files in the public folder available to the world
-app.use(express.static(__dirname + '/public'));
+	// Make the files in the public folder available to the world
+	app.use(express.static(__dirname + '/public'));
+};
 
 
-// Initialize a new socket.io application
+/**
+* handle user disconnecting
+* @returns {undefined}
+*/
+var disconnectHandler = function(socket) {
+	console.log('user '+socket.id+' disconnected');
+};
 
-var room = io.on('connection', function (socket) {
 
-	// A new client has come online. 
-	socket.emit('connectionready');
+/**
+* handle new user entering the room
+* @returns {undefined}
+*/
+var enterHandler = function(socket, data) {
+	var newuserData = data,
+		acceptanceData = {};
 
-	socket.on('disconnect', function(){
-	  console.log('user '+socket.id+' disconnected');
+	console.log('socket enter event ('+data.role+') id:'+data.id);
+
+	//send message to newly accepted user
+	socket.emit('accepted', acceptanceData);
+
+	//send message to rest of the room
+	//room.emit('newuser', newuserData);
+	socket.broadcast.emit('newuser', newuserData);
+};
+
+
+/**
+* handle event that just has to be passed through to all sockets
+* @returns {undefined}
+*/
+var passThroughHandler = function() {
+	
+};
+
+
+/**
+* handle device tilt change
+* @returns {undefined}
+*/
+var tiltchangeHandler = function(data) {
+	room.emit('tiltchange', data);
+};
+
+
+/**
+* create a "room" where all sockets can meet up
+* @returns {undefined}
+*/
+var createRoom = function() {
+	room = io.on('connection', function (socket) {
+
+		// A new client has come online. 
+		socket.emit('connectionready');
+
+		socket.on('disconnect', function(){
+			disconnectHandler(socket);
+		});
+
+		socket.on('enter', function(data) {
+			enterHandler(socket, data);
+		});
+
+		socket.on('tiltchange', tiltchangeHandler);
+
 	});
-
-	socket.on('enter', function(data) {
-		//console.log(data);
-
-		var newuserData = data,
-			acceptanceData = {};
-
-		console.log('socket enter event ('+data.role+') id:'+data.id);
-
-		//send message to newly accepted user
-		socket.emit('accepted', acceptanceData);
-
-		//send message to rest of the room
-		//room.emit('newuser', newuserData);
-		socket.broadcast.emit('newuser', newuserData);
-	});
-
-	socket.on('tiltchange', function(data) {
-
-		room.emit('tiltchange', data);
-	});
-
-});
-
+};
 
 
 /**
@@ -66,7 +109,8 @@ var room = io.on('connection', function (socket) {
 * @returns {undefined}
 */
 var init = function() {
-	
+	initBasicRequirements();
+	createRoom();
 	console.log('Now running on http://localhost:' + port);
 };
 
